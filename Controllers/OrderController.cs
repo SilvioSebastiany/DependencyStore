76118ed8ff29561450a741f0b239dba952df104e
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-24 16:05:43 -0300

    Removendo as regras de negócio do controlador

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -14,7 +14,7 @@ public class OrderController : ControllerBase
     private readonly IDeliveryFeeService _deliveryFeeService;
     private readonly IPromoCodeRepository _promoCodeRepository;
 
-    public OrderController(ICustomerRepository customerRepository, IDeliveryFeeService deliveryFeeService, IPromoCodeRepository promoCodeRepository) 
+    public OrderController(ICustomerRepository customerRepository, IDeliveryFeeService deliveryFeeService, IPromoCodeRepository promoCodeRepository)
     {
         _customerRepository = customerRepository;
         _deliveryFeeService = deliveryFeeService;
@@ -34,46 +34,11 @@ public class OrderController : ControllerBase
         // #2 - Calcula o frete
         var deliveryFee = await _deliveryFeeService.GetDeliveryFeeAsync(zipCode);
         var cupom = await _promoCodeRepository.GetPromoCodeAsync(promoCode);
+        var discount = cupom?.Value ?? 0M;
 
-
-        // #3 - Calcula o total dos produtos
-        decimal subTotal = 0;
-        const string getProductQuery = "SELECT [Id], [Name], [Price] FROM PRODUCT WHERE ID=@id";
-        for (var p = 0; p < products.Length; p++)
-        {
-            Product product;
-            await using (var conn = new SqlConnection("CONN_STRING"))
-                product = await conn.QueryFirstAsync<Product>(getProductQuery, new { Id = p });
-
-            subTotal += product.Price;
-        }
-
-        // #4 - Aplica o cupom de desconto
-        decimal discount = 0;
-        await using (var conn = new SqlConnection("CONN_STRING"))
-        {
-            const string query = "SELECT * FROM PROMO_CODES WHERE CODE=@code";
-            var promo = await conn.QueryFirstAsync<PromoCode>(query, new { code = promoCode });
-            if (promo.ExpireDate > DateTime.Now)
-                discount = promo.Value;
-        }
-
-        // #5 - Gera o pedido
-        var order = new Order();
-        order.Code = Guid.NewGuid().ToString().ToUpper().Substring(0, 8);
-        order.Date = DateTime.Now;
-        order.DeliveryFee = deliveryFee;
-        order.Discount = discount;
-        order.Products = products;
-        order.SubTotal = subTotal;
-
-        // #6 - Calcula o total
-        order.Total = subTotal - discount + deliveryFee;
+        var order = new Order(deliveryFee, discount, new List<Product>());
 
         // #7 - Retorna
-        return Ok(new
-        {
-            Message = $"Pedido {order.Code} gerado com sucesso!"
-        });
+        return Ok( $"Pedido {order.Code} gerado com sucesso!");
     }
 }

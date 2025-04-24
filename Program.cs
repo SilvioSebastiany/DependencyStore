49f5d986c460be95da0e13bde257fa427cf0a615
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-24 16:07:18 -0300

    Resolvendo as dependências - AddTransient e AddScoped

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Contracts;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+
+builder.Services.AddScoped<SqlConnection>(sp => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
